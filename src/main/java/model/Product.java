65ea421a3bd7b042edd5832bec64539bff8e0935
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-09-11 12:50:58 +0200

    Product Class implemented

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,57 @@
 package model;
 
+import java.util.Objects;
+
 public class Product {
+
+
+    private int productId;
+    private String productName;
+
+    public Product(int productId, String productName) {
+        this.productId = productId;
+        this.productName = productName;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (productId != product.productId) return false;
+        return Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = productId;
+        result = 31 * result + (productName != null ? productName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                '}';
+    }
 }
